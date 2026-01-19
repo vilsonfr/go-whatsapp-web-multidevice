cbcc87e12a56f56aa848c8f58af84c952e48311a
Vilson <vilsonfreitaspacheco@gmail.com>
2026-01-19 18:42:27 -0400

    trocando o "AldinoKemal" para "Route Done"

--- src/config/settings.go
+++ src/config/settings.go
@@ -9,7 +9,7 @@ var (
 	AppPort                = "3000"
 	AppHost                = "0.0.0.0"
 	AppDebug               = false
-	AppOs                  = "AldinoKemal"
+	AppOs                  = "Route Done"
 	AppPlatform            = waCompanionReg.DeviceProps_PlatformType(1)
 	AppBasicAuthCredential []string
 	AppBasePath            = ""
